bd164fbf99028352455e993633615ee742625048
sasa3456 <sasastarov30058@gmail.com>
2025-08-25 22:45:32 +0300

    Update

--- Sources/Kernel/Graphics/ULImpl/LoggerAdapters/LoggerDefault.cpp
+++
@@ -1,9 +0,0 @@
-#include "LoggerDefault.hpp"
-#include "../../../Utility/ErrorHandler.hpp"
-#include <Ultralight/String.h>
-
-namespace VE_Kernel
-{
-    void LoggerDefault::LogMessage(ul::LogLevel log_level_a,
-                                   const ul::String16& message_a) {}
-} // namespace VE_Kernel
